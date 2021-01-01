9891d08eb016c69ca4ad19b0b24a080e9c2e5f79
artinx-000 <2603119857@qq.com>
2021-01-01 21:28:09 +0800

    修正åremote地址

--- gitcommit.sh
+++ gitcommit.sh
@@ -3,5 +3,5 @@ read -p "您要commit什么呢?" COMMIT
 git add .
 git commit -m $COMMIT
 git branch -M main
-git remote add origin https://github.com/Artinx-Algorithm-Group/VideoUtils.git
+git remote add origin https://github.com/2catycm/VisualKnowledgeReview_for_Java.git
 git push -u origin main
