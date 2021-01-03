4c7b7e0bfca506f8739b282d13f4b3ec859b93f0
IchFindeDich <2540918998@qq.com>
2021-01-03 13:47:12 +0800

    除以零要分情况 浮点数还是整数

--- src/R_primitiveType/除0.java
+++ src/R_primitiveType/除0.java
@@ -5,5 +5,9 @@ public class 除0 {
         int x = 0;
         int y = 8 / x;
         System.out.println(y);
+
+        System.out.println(2.0/0.0);
+        System.out.println(0.0/0.0);
+        //浮点数运算/0不会报错，0.0/0.0=NaN  非零数/0.0 = INFINITY
     }
 }
